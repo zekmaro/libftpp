c54ae6c2c7e50f5178556f087dbb1fdb5925a854
Andrey Arama <zekmaro@yandex.ru>
2025-11-29 00:38:34 +0100

    feat: move semantics

--- data_structures/pool.hpp
+++ data_structures/pool.hpp
@@ -4,6 +4,8 @@
 #include <type_traits>
 #include <vector>
 
+#define DEFAULT_CAPACITY 256
+
 
 template <typename T>
 class Pool {
@@ -13,6 +15,11 @@ class Pool {
         std::vector<Storage>    buffer_;
         std::vector<bool>       usedSlots_;
         std::size_t             capacity_ = 0;
+
+    private:
+        void destroySlot(std::size_t index) {
+            //
+        }
 
     public:
         class Object {
@@ -31,12 +38,42 @@ class Pool {
                     }
                 }
 
+				Object(Object&& other) noexcept
+					: pool_(other.pool_),
+					index_(other.index_),
+					ptr_(other.ptr_)
+				{
+					other.pool_ = nullptr;
+					other.ptr_ = nullptr;
+				}
+                Object& operator=(Object&& other) noexcept {
+                    if (this != &other) {
+                        if (pool_) {
+                            pool_->destroySlot(index_);
+                        }
+                        pool_ = other.pool_;
+                        ptr_ = other.ptr_;
+                        index_ = other.index_;
+    
+                        other.pool_ = nullptr;
+                        other.ptr_  = nullptr;
+                        other.index_ = static_cast<std::size_t>(-1);
+                    }
+                    return *this;
+                }
+
+                Object(const Object&) = delete;
+                Object& operator=(const Object&) = delete;
+
+
             T* operator->() { return ptr_; }
             T& operator*() { return *ptr_; }
         };
     
     public:
-        Pool() = default;
+        Pool() {
+            this->resize(DEFAULT_CAPACITY);
+        }
         ~Pool();
 
         void resize(std::size_t count) {
