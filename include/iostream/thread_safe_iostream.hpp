e61f15682e5e8ee06a27f69b00a42ac3a2768040
Andrey Arama <zekmaro@yandex.ru>
2026-05-23 19:55:36 +0200

    wip

--- include/iostream/thread_safe_iostream.hpp
+++ include/iostream/thread_safe_iostream.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <sstream>
 
 class ThreadSafeIOStream {
 	std::string prefix_;
@@ -25,6 +26,18 @@ public:
 
 	template<typename T>
     ThreadSafeIOStream& operator<<(const T& value) {
-		
+		std::lock_guard<std::mutex> lock(mutex_);
+		buffer_ << value;
+		size_t new_line_pos_ = buffer_.str().find('\n');
+		while (new_line_pos_ != std::string::npos) {
+			std::string line = buffer_.str().substr(0, new_line_pos_);
+			std::cout << prefix_ << line << "\n";
+			std::string remaining = buffer_.str().substr(new_line_pos_ + 1);
+			buffer_.str("");
+			buffer_.seekp(0);
+			buffer_ << remaining;
+			new_line_pos_ = buffer_.str().find('\n');
+		}
+		return *this;
 	}
 };
